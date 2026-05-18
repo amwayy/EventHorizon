1ed67b5d862a01e7ab79d63ba5b5745c2e91d255
amwayy <42382777+amwayy@users.noreply.github.com>
2026-05-18 20:53:49 +0800

    feat: editor set player position tool

---
+++ Assets/Scripts/PlayerTeleporter.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class PlayerTeleporter : MonoBehaviour
+{
+    [SerializeField] private Transform startPosition;
+    [SerializeField] private Transform hub;
+
+    [ContextMenu("Reset position")]
+    public void ResetPosition()
+    {
+        SetPosition(startPosition);
+    }
+
+    [ContextMenu("Go to hub")]
+    public void GoToHub()
+    {
+        SetPosition(hub);
+    }
+
+    private void SetPosition(Transform point)
+    {
+#if UNITY_EDITOR
+        Undo.RecordObject(transform, "Reset Position");
+#endif
+
+        transform.SetPositionAndRotation(
+            point.position,
+            point.rotation
+        );
+
+#if UNITY_EDITOR
+        EditorUtility.SetDirty(transform);
+#endif
+    }
+}
