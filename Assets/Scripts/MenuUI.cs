d8ab4349ce9ec74320863babf41284e17a097fec
amwayy <42382777+amwayy@users.noreply.github.com>
2026-06-07 16:26:21 +0800

    feat: fullscreen / windowed mode toggle

--- Assets/Scripts/MenuUI.cs
+++ Assets/Scripts/MenuUI.cs
@@ -11,17 +11,23 @@ namespace DefaultNamespace
         [SerializeField] private Button clearSaveDataButton;
         [SerializeField] private Canvas canvas;
         [SerializeField] private TMP_Text versionText;
+        [SerializeField] private Button fullscreenButton;
+        [SerializeField] private Button windowedButton;
         
         private void Awake()
         {
             backButton.onClick.AddListener(Resume);
             quitButton.onClick.AddListener(Quit);
             clearSaveDataButton.onClick.AddListener(ClearSaveData);
+            fullscreenButton.onClick.AddListener(EnterFullscreen);
+            windowedButton.onClick.AddListener(EnterWindowed);
         }
 
         private void Start()
         {
             versionText.text = Application.version;
+            
+            UpdateFullscreenButtons();
         }
 
         private void ClearSaveData()
@@ -37,6 +43,27 @@ namespace DefaultNamespace
         private void Quit()
         {
             Application.Quit();
+        }
+
+        private void UpdateFullscreenButtons()
+        {
+            var isFullscreen = Screen.fullScreenMode is FullScreenMode.ExclusiveFullScreen or FullScreenMode.FullScreenWindow;
+            fullscreenButton.gameObject.SetActive(!isFullscreen);
+            windowedButton.gameObject.SetActive(isFullscreen);
+        }
+
+        private void EnterFullscreen()
+        {
+            Screen.fullScreenMode = FullScreenMode.ExclusiveFullScreen;
+            fullscreenButton.gameObject.SetActive(false);
+            windowedButton.gameObject.SetActive(true);
+        }
+        
+        private void EnterWindowed()
+        {
+            Screen.fullScreenMode = FullScreenMode.Windowed;
+            fullscreenButton.gameObject.SetActive(true);
+            windowedButton.gameObject.SetActive(false);
         }
     }
 }
