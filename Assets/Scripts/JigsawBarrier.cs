d194733dc296635cdf0a5b5e685a64bd43592165
amwayy <42382777+amwayy@users.noreply.github.com>
2026-06-15 22:34:32 +0800

    fix: hub barriers color change

--- Assets/Scripts/JigsawBarrier.cs
+++ Assets/Scripts/JigsawBarrier.cs
@@ -13,7 +13,7 @@ namespace DefaultNamespace
         
         private static readonly int Alpha = Shader.PropertyToID("_Alpha");
         
-        private int _levelId = -1;
+        private int _levelId = -2;
 
         private void Start()
         {
